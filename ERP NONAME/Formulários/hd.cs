464ca72d121b9d00b9f34912156ce8c7162241d5
leoadm <leoadm@ERLI-PC>
2018-01-17 23:05:34 -0300

    Alteração nos formulários

---
+++ ERP NONAME/Formulários/hd.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ALTO_VALE.Formulários
+{
+    public partial class hd : Form
+    {
+        public hd()
+        {
+            InitializeComponent();
+        }
+
+        private void bairroTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ObservacaoTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ObservacaoTextBox_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
