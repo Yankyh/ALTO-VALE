00a8769d9268d76982e100c9367b37feb8332c55
Matheus Yan <matt_yn@outlook.com>
2018-01-18 00:28:39 -0200

    Criando tela de tarefa

---
+++ ERP NONAME/VIEW/TR_TAREFA/ITarefa.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace ALTO_VALE.VIEW.TR_TAREFA
+{
+    partial class ITarefa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.assuntoTextBox = new System.Windows.Forms.TextBox();
+            this.solicitanteTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.observacaoTabPage = new System.Windows.Forms.TabPage();
+            this.solicitacaoTextBox = new System.Windows.Forms.RichTextBox();
+            this.tipoComboBox = new System.Windows.Forms.ComboBox();
+            this.situacaoComboBox = new System.Windows.Forms.ComboBox();
+            this.severidadeComboBox = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.numeroTextBox = new System.Windows.Forms.TextBox();
+            this.dataTextBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.prazoTextBox = new System.Windows.Forms.TextBox();
+            this.liberarButton = new System.Windows.Forms.Button();
+            this.voltarButton = new System.Windows.Forms.Button();
+            this.cancelarButton = new System.Windows.Forms.Button();
+            this.gravarButton = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.observacaoTabPage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(682, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 16);
+            this.label4.TabIndex = 62;
+            this.label4.Text = "Severidade";
+            // 
+            // assuntoTextBox
+            // 
+            this.assuntoTextBox.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.assuntoTextBox.Location = new System.Drawing.Point(7, 136);
+            this.assuntoTextBox.Name = "assuntoTextBox";
+            this.assuntoTextBox.Size = new System.Drawing.Size(965, 22);
+            this.assuntoTextBox.TabIndex = 59;
+            // 
+            // solicitanteTextBox
+            // 
+            this.solicitanteTextBox.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.solicitanteTextBox.Location = new System.Drawing.Point(8, 29);
+            this.solicitanteTextBox.Name = "solicitanteTextBox";
+            this.solicitanteTextBox.Size = new System.Drawing.Size(235, 22);
+            this.solicitanteTextBox.TabIndex = 58;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(5, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 16);
+            this.label3.TabIndex = 57;
+            this.label3.Text = "Assunto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(464, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 16);
+            this.label2.TabIndex = 56;
+            this.label2.Text = "Situação";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(246, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 16);
+            this.label1.TabIndex = 55;
+            this.label1.Text = "Tipo";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.Color.Transparent;
+            this.label13.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(5, 10);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(69, 16);
+            this.label13.TabIndex = 54;
+            this.label13.Text = "Solicitante";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.observacaoTabPage);
+            this.tabControl1.Font = new System.Drawing.Font("Arial", 10F);
+            this.tabControl1.Location = new System.Drawing.Point(7, 170);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(968, 199);
+            this.tabControl1.TabIndex = 53;
+            // 
+            // observacaoTabPage
+            // 
+            this.observacaoTabPage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.observacaoTabPage.Controls.Add(this.solicitacaoTextBox);
+            this.observacaoTabPage.Location = new System.Drawing.Point(4, 25);
+            this.observacaoTabPage.Name = "observacaoTabPage";
+            this.observacaoTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.observacaoTabPage.Size = new System.Drawing.Size(960, 170);
+            this.observacaoTabPage.TabIndex = 1;
+            this.observacaoTabPage.Text = "Solicitação";
+            this.observacaoTabPage.UseVisualStyleBackColor = true;
+            // 
+            // solicitacaoTextBox
+            // 
+            this.solicitacaoTextBox.Location = new System.Drawing.Point(1, 0);
+            this.solicitacaoTextBox.Name = "solicitacaoTextBox";
+            this.solicitacaoTextBox.Size = new System.Drawing.Size(955, 166);
+            this.solicitacaoTextBox.TabIndex = 0;
+            this.solicitacaoTextBox.Text = "";
+            // 
+            // tipoComboBox
+            // 
+            this.tipoComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.tipoComboBox.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tipoComboBox.FormattingEnabled = true;
+            this.tipoComboBox.Location = new System.Drawing.Point(249, 28);
+            this.tipoComboBox.Name = "tipoComboBox";
+            this.tipoComboBox.Size = new System.Drawing.Size(212, 23);
+            this.tipoComboBox.TabIndex = 64;
+            // 
+            // situacaoComboBox
+            // 
+            this.situacaoComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.situacaoComboBox.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.situacaoComboBox.FormattingEnabled = true;
+            this.situacaoComboBox.Location = new System.Drawing.Point(467, 28);
+            this.situacaoComboBox.Name = "situacaoComboBox";
+            this.situacaoComboBox.Size = new System.Drawing.Size(212, 23);
+            this.situacaoComboBox.TabIndex = 65;
+            // 
+            // severidadeComboBox
+            // 
+            this.severidadeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.severidadeComboBox.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.severidadeComboBox.FormattingEnabled = true;
+            this.severidadeComboBox.Location = new System.Drawing.Point(685, 28);
+            this.severidadeComboBox.Name = "severidadeComboBox";
+            this.severidadeComboBox.Size = new System.Drawing.Size(178, 23);
+            this.severidadeComboBox.TabIndex = 66;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(866, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 16);
+            this.label7.TabIndex = 67;
+            this.label7.Text = "Número";
+            // 
+            // numeroTextBox
+            // 
+            this.numeroTextBox.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numeroTextBox.Location = new System.Drawing.Point(868, 28);
+            this.numeroTextBox.Name = "numeroTextBox";
+            this.numeroTextBox.Size = new System.Drawing.Size(107, 22);
+            this.numeroTextBox.TabIndex = 68;
+            // 
+            // dataTextBox
+            // 
+            this.dataTextBox.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataTextBox.Location = new System.Drawing.Point(8, 84);
+            this.dataTextBox.Name = "dataTextBox";
+            this.dataTextBox.Size = new System.Drawing.Size(128, 22);
+            this.dataTextBox.TabIndex = 69;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(5, 65);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 16);
+            this.label8.TabIndex = 70;
+            this.label8.Text = "Data";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(139, 65);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(42, 16);
+            this.label9.TabIndex = 72;
+            this.label9.Text = "Prazo";
+            // 
+            // prazoTextBox
+            // 
+            this.prazoTextBox.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.prazoTextBox.Location = new System.Drawing.Point(142, 84);
+            this.prazoTextBox.Name = "prazoTextBox";
+            this.prazoTextBox.Size = new System.Drawing.Size(128, 22);
+            this.prazoTextBox.TabIndex = 73;
+            // 
+            // liberarButton
+            // 
+            this.liberarButton.Font = new System.Drawing.Font("Arial", 10F);
+            this.liberarButton.Location = new System.Drawing.Point(558, 375);
+            this.liberarButton.Name = "liberarButton";
+            this.liberarButton.Size = new System.Drawing.Size(103, 32);
+            this.liberarButton.TabIndex = 77;
+            this.liberarButton.Text = "Liberar";
+            this.liberarButton.UseVisualStyleBackColor = true;
+            this.liberarButton.Visible = false;
+            // 
+            // voltarButton
+            // 
+            this.voltarButton.Font = new System.Drawing.Font("Arial", 10F);
+            this.voltarButton.Location = new System.Drawing.Point(662, 375);
+            this.voltarButton.Name = "voltarButton";
+            this.voltarButton.Size = new System.Drawing.Size(103, 32);
+            this.voltarButton.TabIndex = 76;
+            this.voltarButton.Text = "Voltar";
+            this.voltarButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelarButton
+            // 
+            this.cancelarButton.Font = new System.Drawing.Font("Arial", 10F);
+            this.cancelarButton.Location = new System.Drawing.Point(768, 375);
+            this.cancelarButton.Name = "cancelarButton";
+            this.cancelarButton.Size = new System.Drawing.Size(103, 32);
+            this.cancelarButton.TabIndex = 74;
+            this.cancelarButton.Text = "Cancelar";
+            this.cancelarButton.UseVisualStyleBackColor = true;
+            // 
+            // gravarButton
+            // 
+            this.gravarButton.Font = new System.Drawing.Font("Arial", 10F);
+            this.gravarButton.Location = new System.Drawing.Point(872, 375);
+            this.gravarButton.Name = "gravarButton";
+            this.gravarButton.Size = new System.Drawing.Size(103, 32);
+            this.gravarButton.TabIndex = 75;
+            this.gravarButton.Text = "Gravar";
+            this.gravarButton.UseVisualStyleBackColor = true;
+            // 
+            // ITarefa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 411);
+            this.Controls.Add(this.liberarButton);
+            this.Controls.Add(this.voltarButton);
+            this.Controls.Add(this.cancelarButton);
+            this.Controls.Add(this.gravarButton);
+            this.Controls.Add(this.prazoTextBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.dataTextBox);
+            this.Controls.Add(this.numeroTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.severidadeComboBox);
+            this.Controls.Add(this.situacaoComboBox);
+            this.Controls.Add(this.tipoComboBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.assuntoTextBox);
+            this.Controls.Add(this.solicitanteTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "ITarefa";
+            this.Text = "Tarefa";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.tarefaFormClosed);
+            this.tabControl1.ResumeLayout(false);
+            this.observacaoTabPage.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox assuntoTextBox;
+        private System.Windows.Forms.TextBox solicitanteTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage observacaoTabPage;
+        private System.Windows.Forms.ComboBox tipoComboBox;
+        private System.Windows.Forms.ComboBox situacaoComboBox;
+        private System.Windows.Forms.ComboBox severidadeComboBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox numeroTextBox;
+        private System.Windows.Forms.TextBox dataTextBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox prazoTextBox;
+        private System.Windows.Forms.Button liberarButton;
+        private System.Windows.Forms.Button voltarButton;
+        private System.Windows.Forms.Button cancelarButton;
+        private System.Windows.Forms.Button gravarButton;
+        private System.Windows.Forms.RichTextBox solicitacaoTextBox;
+    }
+}
