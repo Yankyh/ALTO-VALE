784ee11c614e2cfcf7e9a606192364875bf1d384
Matheus Yan <matt_yn@outlook.com>
2018-01-23 21:31:26 -0200

    Alterando menu

--- ERP NONAME/VIEW/MD_SISTEMA/IMenu.cs
+++ ERP NONAME/VIEW/MD_SISTEMA/IMenu.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ALTO_VALE.DAL;
 using ALTO_VALE.TN_TECNOLOGIA;
 using ALTO_VALE.TN_TECNOLOGIA.Controles;
 //using ALTO_VALE.TN_TECNOLOGIA.Controles;
@@ -21,10 +22,11 @@ namespace ALTO_VALE
     public partial class Form1 : Form
     {
         // ControleTelaMenu controleTelaMenu = new ControleTelaMenu();
-        
+        Connection connection = new Connection();
         public Form1()
         {
             InitializeComponent();
+            connection.Conectar();
             menuTreeView.Visible = false;
         }
 
@@ -45,7 +47,7 @@ namespace ALTO_VALE
 
             if (nodeSelecionado != "")
             {
-                ControleTelas(nodeSelecionado);
+                GerenciarMenuDataGridView(nodeSelecionado);
             }
 
         }
@@ -92,7 +94,7 @@ namespace ALTO_VALE
 
         private void MenuFormDeactivate(object sender, EventArgs e)
         {
-            menuButton.Location = new Point(1,46);
+            menuButton.Location = new Point(1, 46);
             menuTreeView.Visible = false;
         }
 
@@ -135,13 +137,121 @@ namespace ALTO_VALE
         private void button2_Click(object sender, EventArgs e)
         {
             ControleDadosMenu controleDadosMenu = new ControleDadosMenu();
-           // controleDadosMenu.GerenciarMenuDataGridView();
+            // controleDadosMenu.GerenciarMenuDataGridView();
         }
 
         private void button3_Click_1(object sender, EventArgs e)
         {
             PADRONIZACAO.Form1 form = new PADRONIZACAO.Form1();
             form.ShowDialog();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        //Controle do datagridview
+        private void GerenciarMenuDataGridView(String tela)
+        {
+            String query = "";
+            BindingSource Binding = new BindingSource();
+
+            //Pessoa
+            if (tela == "Pessoa")
+            {
+                menuDataGridView.AutoGenerateColumns = true;
+                query = " SELECT B.NOME SITUAÇÃO, A.RAZAOSOCIAL AS 'RAZÃO SOCIAL', A.APELIDO, A.CPFCNPJ AS 'CPF/CNPJ', A.TELEFONE, C.CIDADE, D.SIGLA ESTADO, C.LOGRADOURO" +
+                               " FROM PS_PESSOA A" +
+                               " INNER JOIN MD_STATUS B ON B.HANDLE = A.STATUS " +
+                               " LEFT JOIN PS_PESSOAENDERECO C ON C.HANDLE = A.ENDERECO" +
+                               " LEFT JOIN MD_ESTADO D ON D.HANDLE = C.ESTADO";
+                Binding.DataSource = connection.DataTable(query);
+                menuDataGridView.DataSource = Binding;
+
+                menuDataGridView.Columns[0].Width = 150;
+                menuDataGridView.Columns[1].Width = 300;
+                menuDataGridView.Columns[2].Width = 300;
+                menuDataGridView.Columns[3].Width = 150;
+                menuDataGridView.Columns[4].Width = 130;
+                menuDataGridView.Columns[7].Width = 230;
+                menuDataGridView.AllowUserToResizeRows = false;
+            }
+            if (tela == "Endereço")
+            {
+                IPessoaEndereco iPessaEndereco = new IPessoaEndereco();
+                iPessaEndereco.ShowDialog();
+            }
+            if (tela == "Contato")
+            {
+                IPessoaContato iPessoaContato = new IPessoaContato();
+                iPessoaContato.ShowDialog();
+            }
+            //Tarefa
+            if (tela == "Tarefa")
+            {
+                query = " SELECT A.HANDLE, B.IMAGEM STA, F.NOME SITUAÇÃO, E.NOME SEVERIDADE, G.NOME TIPO, A.PRAZO, A.ASSUNTO, C.LOGIN SOLICITANTE, D.LOGIN RESPONSAVEL, A.DATA " +
+                        " FROM TR_TAREFA A" +
+                        " INNER JOIN MD_STATUS B ON B.HANDLE = A.STATUS" +
+                        " INNER JOIN PS_USUARIO C ON C.HANDLE = A.SOLICITANTE" +
+                        " INNER JOIN PS_USUARIO D ON D.HANDLE = A.RESPONSAVEL" +
+                        " INNER JOIN TR_TAREFASEVERIDADE E ON E.HANDLE = A.SEVERIDADE" +
+                        " INNER JOIN TR_TAREFASITUACAO F ON F.HANDLE = A.SITUACAO" +
+                        " INNER JOIN TR_TAREFATIPO G ON G.HANDLE = A.TIPO";
+                       //  " WHERE A.STATUS IN (" + status + ")";
+                Binding.DataSource = connection.DataTable(query);
+                menuDataGridView.DataSource = Binding;
+
+                menuDataGridView.Columns[0].Width = 0;
+                menuDataGridView.Columns[0].Visible = false;
+                menuDataGridView.Columns[1].Width = 50;
+                menuDataGridView.Columns[2].Width = 120;
+                menuDataGridView.Columns[3].Width = 120;
+                menuDataGridView.Columns[4].Width = 150;
+                menuDataGridView.Columns[5].Width = 150;
+                menuDataGridView.Columns[6].Width = 850;
+                menuDataGridView.Columns[7].Width = 120;
+                menuDataGridView.Columns[8].Width = 130;
+                menuDataGridView.Columns[9].Width = 150;
+
+                menuDataGridView.AllowUserToResizeRows = false;
+            }
+            if (tela == "Servidor de Email")
+            {
+                VIEW.TN_EMAIL.IServidorEmail servidor = new VIEW.TN_EMAIL.IServidorEmail();
+                servidor.ShowDialog();
+            }
+            //Cep
+            if (tela == "Cep")
+            {
+                menuDataGridView.AutoGenerateColumns = true;
+                query = " SELECT B.NOME, A.CEP, A.PAIS, A.ESTADO, A.CIDADE, A.BAIRRO, A.LOGRADOURO, A.HANDLE" +
+                               " FROM MD_CEP A" +
+                               " INNER JOIN MD_STATUS B ON B.HANDLE = A.STATUS";
+                Binding.DataSource = connection.DataTable(query);
+                menuDataGridView.DataSource = Binding;
+
+                menuDataGridView.Columns[0].Width = 150;
+                menuDataGridView.Columns[1].Width = 150;
+                menuDataGridView.Columns[2].Width = 150;
+                menuDataGridView.Columns[3].Width = 150;
+                menuDataGridView.Columns[4].Width = 200;
+                menuDataGridView.Columns[6].Width = 300;
+                menuDataGridView.Columns[7].Visible = false;
+                menuDataGridView.AllowUserToResizeRows = false;
+            }
+        }
+
+        private void MenuFormClosed(object sender, FormClosedEventArgs e)
+        {
+            connection.Desconectar();
         }
     }
 }
